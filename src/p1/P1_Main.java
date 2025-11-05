c91bee99c56ffa5f272c63d4bba84926407b1e15
Deniz Mert Aygün <dmert0209@gmail.com>
2025-11-05 14:21:10 +0100

    Aufgabe3.1

---
+++ src/p1/P1_Main.java
@@ -0,0 +1,24 @@
+package p1;
+
+public class P1_Main {
+
+	public static void main(String[] args) {
+		int wuerfel = 2;
+		
+		if (wuerfel == 1 || wuerfel == 4) {
+			System.out.println("Gewonnen!");
+		}
+		else if (wuerfel ==2 ) {
+			System.out.println("Verloren!");
+		}
+		else if (wuerfel == 5) {
+			System.out.println("Unentschieden!");
+		}
+		else {
+			System.out.println("Unklar");
+		}
+		
+
+	}
+
+}
