e7c20fa10efddab3f23ee56d2bd3351853c2c6fc
Deniz Mert Aygün <dmert0209@gmail.com>
2025-11-06 17:32:56 +0100

    Aufgabe 3.5

---
+++ src/h2/H2_main.java
@@ -0,0 +1,20 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		int jahr = 2025;
+		boolean schalt;
+		
+		if (jahr % 4 == 0 && jahr % 100 != 0|| jahr % 400 == 0) {
+			schalt = true;
+		}
+		else {
+			schalt = false;
+		}
+		
+		System.out.println("Ist ihr Jahr ein Schaltjahr?: " + schalt);
+		
+		}
+		
+}
